9fcf29da75a38ceb46d0aafaf662a025540b9970
deepak <deepak16072000@gmail.com>
2021-10-01 06:05:46 -0700

    starting project

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+//import Card from './card';
+import 'tachyons';
+//import {robots} from './robots' ;
+import App from './App' ;
+ReactDOM.render(<App/>,document.getElementById('root')
+);
+
+
+reportWebVitals();
